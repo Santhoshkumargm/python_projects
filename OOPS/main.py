a9ec713d8afa2eee0869a103600b6e921cab522b
Santhoshkumargm <santhoshkumargm@gmail.com>
2022-01-31 15:37:49 +0530

    .

---
+++ OOPS/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle
+from turtle import Screen
+
+my_turtle = Turtle()
+print(my_turtle)
+my_turtle.shape("turtle")
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
