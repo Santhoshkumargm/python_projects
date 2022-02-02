fad3a6bfc2fb4d28fd3ccbdeb4e72ab5a9e50dd4
Santhoshkumargm <santhoshkumargm@gmail.com>
2022-02-02 11:18:27 +0530

    .

--- Quiz_Game/main.py
+++ Quiz_Game/main.py
@@ -1,18 +1,17 @@
-from data import question_data
 from quiz_brain import QuizBrain
 from question_model import Question
+from data import question_data
 
 question_bank = []
-for x in range(len(question_data)):
-    new_question = Question(question_data[x]["question"], question_data[x]["correct_answer"])
+x = len(question_data)
+for i in range(x):
+    new_question = Question(question_data[i]["text"], question_data[i]["answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
-    quiz.next_question()
+    final_score = quiz.next_question()
 
 print("You've completed the quiz.")
 print(f"Your final score is {quiz.score}/{quiz.question_number}")
-
-
